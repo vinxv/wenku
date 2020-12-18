e0e38e9ce613b90ca415062636f084c5161eb89d
vincent <tushan1017@gmail.com>
2020-12-18 12:11:19 +0800

    修正因文库网站更新导致文档无法下载的问题。

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wenku",
-    version="0.24",
+    version="0.25",
     packages=find_packages(),
 
     scripts=['./wenku.py'],
--- wenku.py
+++ wenku.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+from enum import Flag
 import json
 import re
 import time
@@ -13,23 +14,33 @@ import requests
 
 
 class ProgressInfo(ContextDecorator):
+    start_info: str
+    finish_info: str
+    error_info: str
 
-    def __init__(self,
-                 start_info: str = "Starting",
-                 finish_info: str = "Finished"):
+    def __init__(
+        self,
+        start_info: str = "Starting",
+        finish_info: str = "Finished",
+        error_info: str = "Failed",
+    ):
 
         self.t0 = time.time()
         self.start_info = start_info
         self.finish_info = finish_info
+        self.error_info = error_info
         super().__init__()
 
     def __enter__(self):
         print(self.start_info.ljust(20), end="\t")
         return self
 
-    def __exit__(self, *exc):
+    def __exit__(self, exc_type, exc_value, traceback):
         elapse = (time.time() - self.t0) / 1000
-        print(f'{ self.finish_info }\t 耗时:{ elapse :0.5f}s')
+        if exc_value:
+            print(f"{ self.error_info.ljust(10) } 耗时:{ elapse :0.5f}s\t原因:{ exc_value}")
+            return True
+        print(f"{ self.finish_info.ljust(10) } 耗时:{ elapse :0.5f}s")
         return False
 
 
@@ -44,15 +55,15 @@ class WenKuClient:
             content = content.decode("u8")
 
         # find the first '(' and the last ')' and extract the substr between them
-        lindex = content.find('(')
-        rindex = content.rindex(')')
-        return json.loads(content[lindex + 1: rindex])
+        lindex = content.find("(")
+        rindex = content.rindex(")")
+        return json.loads(content[lindex + 1 : rindex])
 
     @staticmethod
     def parse_doc_id(url: str) -> Optional[str]:
         """parse doc_id from url with regex"""
 
-        pat = re.compile(r'([0-9a-f]{8,})')
+        pat = re.compile(r"([0-9a-f]{8,})")
         result = pat.findall(url)
         if result:
             return result[0]
@@ -90,8 +101,8 @@ class WenKuClient:
             }
         ```
         """
-        url = 'https://wenku.baidu.com/api/doc/getdocinfo'
-        params = {'callback': 'cb', 'doc_id': doc_id}
+        url = "https://wenku.baidu.com/api/doc/getdocinfo"
+        params = {"callback": "cb", "doc_id": doc_id}
         content = requests.get(url, params=params).content
         return cls.load_jsonp(content)
 
@@ -102,15 +113,15 @@ class WenKuClient:
         we need 'bcsParam' and 'md5sum' from doc_info to form image url
         """
 
-        md5sum = doc_info['md5sum']
-        doc_id = doc_info['doc_id']
-        pat = re.compile(r'png=(\d+-\d+)&jpg=(\d+-\d+)')
+        md5sum = doc_info["md5sum"]
+        doc_id = doc_info["doc_id"]
+        pat = re.compile(r"png=(\d+-\d+)&jpg=(\d+-\d+)")
 
         urls = []
-        for param in doc_info['bcsParam']:
-            img_fmt = 'jpg'
+        for param in doc_info["bcsParam"]:
+            img_fmt = "jpg"
 
-            zoom: str = param['zoom']
+            zoom: str = param["zoom"]
             res = pat.findall(zoom)
 
             if not res:
@@ -119,41 +130,51 @@ class WenKuClient:
             png_range, jpg_range = res[0]
 
             # when doc is in format of word or txt, it has no images
-            if jpg_range == '0-0' and png_range == '0-0':
+            if jpg_range == "0-0" and png_range == "0-0":
                 continue
 
             # png image
-            if jpg_range == '0-0':
-                img_fmt = 'png'
-            url = f'https://wkretype.bdimg.com/retype/zoom/{doc_id}?o={ img_fmt }{ md5sum }{ zoom }'
+            if jpg_range == "0-0":
+                img_fmt = "png"
+            url = f"https://wkretype.bdimg.com/retype/zoom/{doc_id}?o={ img_fmt }{ md5sum }{ zoom }"
             urls.append(url)
         return urls
 
     @classmethod
     def parse_doc_content(cls, raw: bytes) -> str:
-        """parse text from doc info
-        """
-        items = cls.load_jsonp(raw)['body']
-        text = "".join([item['c'] for item in items if item['t'] == 'word'])
+        """parse text from doc info"""
+        items = cls.load_jsonp(raw)["body"]
+        last_y = None
+        text = ""
+        for item in items:
+            if not item['t'] == "word":
+                continue
+            text += item['c']
+            y = item['p']['y']
+            if last_y is not None and abs(y - last_y) > 1:
+                text += "\n"
+            last_y = y
         return text
 
     @classmethod
     @ProgressInfo("正在获取文档文本...")
     def get_text(cls, doc_id: dict) -> str:
         """get text of doc_id"""
-        url = f'https://wenku.baidu.com/view/{ doc_id }'
-        html = cls.session.get(url).content.decode('gb2312')
-
-        pat = re.compile(r'WkInfo\.htmlUrls\s=\s\'(.*\})', re.IGNORECASE)
+        url = f"https://wenku.baidu.com/view/{ doc_id }"
+        html = cls.session.get(url).text
+        pat = re.compile(r"var pageData = (\{.*\})")
         res = pat.findall(html)
         if not res:
-            return ''
+            raise ValueError("无法解析页面")
 
-        json_text = res[0]
-        json_text = json_text.replace(r"\x22", '"').replace(r'\\', '')
-        doc: dict = json.loads(json_text)
-        content_info: list = doc['json']
-        urls = [item['pageLoadUrl'] for item in content_info]
+        page_data = json.loads(res[0])
+        html_url_content = page_data["readerInfo2019"]["htmlUrls"]
+        html_urls = json.loads(html_url_content)
+        if isinstance(html_urls, list):
+            raise ValueError("非文本类型文档")
+
+        url_infos = html_urls["json"]
+        urls = [info["pageLoadUrl"] for info in url_infos]
         frags = []
         for data in cls.batch_fetch(urls):
             text = cls.parse_doc_content(data)
@@ -166,14 +187,16 @@ class WenKuClient:
         image_urls = cls.parse_image_urls(doc_info)
         results = cls.batch_fetch(image_urls)
         for idx, data in enumerate(results, 1):
-            ext = 'jpg' if 'o=jpg' in image_urls[idx-1] else 'png'
-            yield f'page-{ idx }.{ ext }', data
+            ext = "jpg" if "o=jpg" in image_urls[idx - 1] else "png"
+            yield f"page-{ idx }.{ ext }", data
 
     @classmethod
     def batch_fetch(cls, urls: List[str]) -> Iterable[bytes]:
         """fetch content from batch urls concurrently and in order by ThreadPoolExecutor"""
+
         def get_content(url: str) -> bytes:
-            return requests.get(url).content
+            c = requests.get(url).content
+            return c
 
         tasks = []
         with futures.ThreadPoolExecutor(max_workers=cls.concurrency) as executor:
@@ -201,21 +224,26 @@ class WenKuClient:
 
         doc_info = cls.get_doc_info(doc_id)
 
-        title = doc_info['docInfo']['docTitle']
+        title = doc_info["docInfo"]["docTitle"]
         dirpath = cls.mkdir(title)
 
         for filename, data in cls.get_images(doc_info):
-            with open(f"{title}/{filename}", 'wb') as f:
+            with open(f"{title}/{filename}", "wb") as f:
                 f.write(data)
 
-        with open(f'{title}/{ title }.txt', 'w') as f:
-            f.write(cls.get_text(doc_id))
+        text = cls.get_text(doc_id)
+        if text is None:
+            warnings.warn("无文本信息")
+            return
+
+        with open(f"{title}/{ title }.txt", "w") as f:
+            f.write(text)
         print(f"文档下载到目录: { dirpath }")
 
 
 if __name__ == "__main__":
 
-    description = '''Baidu Wenku Downloader. Usage: wenku.py <url>'''
+    description = """Baidu Wenku Downloader. Usage: wenku.py <url>"""
     cmd = argparse.ArgumentParser(description=description)
     cmd.add_argument("url", type=str)
     args = cmd.parse_args()
